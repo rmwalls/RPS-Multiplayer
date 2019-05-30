723e62d776d432a8b9d1fd5419fe195d76fa8cd5
ZelleM <groovyrlm@gmail.com>
2019-05-29 21:19:39 -0400

    start of html wireframe, addition of Firebase database

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyBk5s_9naYWIzHwcJgRXUCCTd_8qlc2N8o",
+    authDomain: "rock-paper-scissors-for-2.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-for-2.firebaseio.com",
+    projectId: "rock-paper-scissors-for-2",
+    storageBucket: "rock-paper-scissors-for-2.appspot.com",
+    messagingSenderId: "724067654127",
+    appId: "1:724067654127:web:2da4d242e2502f72"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
